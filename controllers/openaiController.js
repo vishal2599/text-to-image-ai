e399d738429fcb68ba1d7c883c91a3ac4f05d74b
vishal2599 <vshl2599@gmail.com>
2022-12-22 18:09:49 +0530

    OpenAI backend Completed

---
+++ controllers/openaiController.js
@@ -0,0 +1,40 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const generateImage = async (req, res) => {
+    const { prompt, size, n } = req.body;
+
+    const imageSize = size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024';
+    const numberOfImages = n > 6 ? 6 : n;
+
+    try {
+        const response = openai.createImage({
+            prompt,
+            n: numberOfImages,
+            size: imageSize
+        });
+        const imageUrl = (await response).data.data[0].url;
+        res.status(200).json({
+            success: true,
+            data: imageUrl
+        })
+    } catch (error) {
+        if (error.response) {
+            console.log(error.response.status);
+            console.log(error.response.data);
+          } else {
+            console.log(error.message);
+          }
+
+        res.status(200).json({
+            success: false,
+            error: 'The image could not be generated'
+        })
+    }
+}
+
+module.exports = { generateImage }
